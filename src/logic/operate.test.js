067326109dd1a16d071efbe6d22e4be133df1900
Oscar-Espinoza <oscardavid2094@gmail.com>
2023-01-18 12:14:57 -0300

    Calculate test done

---
+++ src/logic/operate.test.js
@@ -0,0 +1,27 @@
+import operate from './operate';
+
+describe('operate', () => {
+  test('adds two numbers', () => {
+    expect(operate(1, 2, '+')).toBe('3');
+  });
+
+  test('subtracts two numbers', () => {
+    expect(operate(5, 3, '-')).toBe('2');
+  });
+
+  test('multiplies two numbers', () => {
+    expect(operate(4, 5, 'x')).toBe('20');
+  });
+
+  test('divides two numbers', () => {
+    expect(operate(10, 2, '÷')).toBe('5');
+  });
+
+  test('finds modulus of two numbers', () => {
+    expect(operate(5, 3, '%')).toBe('2');
+  });
+
+  test('throws error for unknown operation', () => {
+    expect(() => operate(1, 2, '$')).toThrow('Unknown operation');
+  });
+});
