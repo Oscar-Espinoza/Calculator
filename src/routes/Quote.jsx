30702c18ec74d486709591fa8e06197331c74907
Oscar-Espinoza <oscardavid2094@gmail.com>
2023-01-17 23:43:41 -0300

    Home, calculator and Quote page created, style added to all of them

---
+++ src/routes/Quote.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable max-len */
+export default function Quote() {
+  return (
+    <div className="quote-container">
+      <p className="quote">
+        <q>Mathematics is the queen of the sciences and number theory is the queen of mathematics.</q>
+        - Carl Friedrich Gauss
+      </p>
+
+    </div>
+  );
+}
